fd9652a123053045e93996554263de86228762eb
timp21337 <timp21337@paneris.org>
2015-07-28 00:37:11 +0100

    Remove unused constructor

---
+++ src/main/java/com/github/timp/anagram/AnagramKeyTree.java
@@ -0,0 +1,10 @@
+package com.github.timp.anagram;
+
+/**
+ * A Tree which contains keys to Anagrams.
+ */
+public class AnagramKeyTree extends Tree<String>{
+  public AnagramKeyTree() {
+    super(null);
+  }
+}
