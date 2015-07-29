abef6059dc457882c5948dfed900d46303b2a809
timp21337 <timp21337@paneris.org>
2015-07-29 21:04:37 +0100

    Remove unused methods

--- src/main/java/com/github/timp/anagram/Anagrammer.java
+++ src/main/java/com/github/timp/anagram/Anagrammer.java
@@ -68,49 +68,4 @@ public class Anagrammer {
       }
   }
 
-
-
-
-  public static int permutation(String str) {
-    return permutation("", str);
-  }
-
-  private static int permutation(String prefix, String str) {
-    int n = str.length();
-    if (n == 0) {
-      //System.out.println(prefix);
-      return 1;
-    } else {
-      int ret = 0;
-      for (int i = 0; i < n; i++) {
-        ret += permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i + 1, n));
-      }
-      return ret;
-    }
-  }
-
-  // TODO Delete
-  public int prunedPermutations(String str) {
-    return prunedPermutations("", str);
-  }
-
-  // TODO Delete
-  private int prunedPermutations(String prefix, String str) {
-    int n = str.length();
-    if (n == 0) {
-      //System.out.println(prefix);
-      return 1;
-    } else {
-      int ret = 0;
-      for (int i = 0; i < n; i++) {
-        String nextPrefix =  prefix + str.charAt(i);
-        System.out.println(nextPrefix + ": " + Arrays.toString(dictionary.lookup(nextPrefix)));
-        if (dictionary.lookup(nextPrefix).length != 0) {
-          ret += prunedPermutations(nextPrefix, str.substring(0, i) + str.substring(i + 1, n));
-        }
-      }
-      return ret;
-    }
-  }
-
 }
